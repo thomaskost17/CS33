75a92fc2c454d0b8c92b60c83fd378935b1b75db
thomaskost17 <kost@TK-LAPTOP.localdomain>
2020-02-09 23:37:57 -0800

    final pexex changes and some hw4 files, fpwr ready for testing

---
+++ hw4/fpwr2.c
@@ -0,0 +1,49 @@
+static float u2f(unsigned u)
+{
+	union floatAndUnsinged
+	{
+		float f;
+		unsigned un;
+	} x;
+	x.un = u;
+
+	return x.f;
+}
+
+float fpwr2(int x)
+{
+	// get result for exponent and fraction
+	// note that we only need to set one bit in the frac as this is always going to be a power of 2
+	unsigned exp, frac;
+	unsigned u;
+	int EXP_MIN = -126;
+	if(x <EXP_MIN-23)  //WE CAN GO TO 2^-149 because of the fact that frac can have leading zeros
+	{
+		//return 0.0
+		exp =0; 
+		frac = 0;
+
+	}else if(x< EXP_MIN) //less than -126, more than -149
+	{
+		//denormalized result
+		exp =0;
+		frac =(1<<(22-(x+127)));//put a 1 x+127 bits from 
+
+	}else if(x < 255)
+	{
+		//normalized result
+		exp = x-127;
+		frac = 0;//need nothing here as leading bit is implied
+	} else
+	{
+		// return +oo
+		exp = 255;
+		frac = 0;//defined as +oo
+
+	}
+	//pack values into 32 bit value
+	
+	u = exp<<23|frac;
+	reutrn u2f(u);
+	
+}
